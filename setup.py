3048554035ff50ccdd0751dfb13905313ca153e6
garrach <garrach76@gmail.com>
2026-06-15 05:55:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ import sys
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
+print(BASE_DIR)
+print(DATA_DIR)
 DIRS = [
     DATA_DIR,
     os.path.join(DATA_DIR, "uploads"),
